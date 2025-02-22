57851f2530c47503a34af6f7cc4339e89764545b
Ajaneesh Rajashekharaiah <ajaneesh.rajashekharaiah@ihx.in>
2025-02-22 06:57:39 +0000

    turned ensure to nil for all packages and moved installation to nix

--- modules/common/emacs/default.nix
+++ modules/common/emacs/default.nix
@@ -15,6 +15,32 @@
         programs.emacs = {
             enable = true;
 	    extraPackages = epkgs: with epkgs; [
+	    which-key
+	    ligature
+	    pyvenv
+	    all-the-icons
+	    evil
+	    modus-themes
+	    undo-tree
+	    evil-collection
+	    evil-surround
+	    evil-commentary
+	    evil-matchit
+	    expand-region
+	    lsp-pyright
+	    vertico
+	    marginalia
+	    consult-lsp
+	    wgrep
+	    orderless
+	    cape
+	    corfu
+	    clojure-mode
+	    cider
+	    flycheck-clj-kondo
+	    json-mode
+	    yafolding
+	    nix-mode
         magit
         transient
         magit-section
