20087b3917f1221fd471fa50456935303b5cba4a
ajaneesh <ajaneeshsr@gmail.com>
2025-07-22 13:09:40 +0530

    moved npm global install outside of nix store

--- modules/common/programming/nodejs.nix
+++ modules/common/programming/nodejs.nix
@@ -6,4 +6,13 @@
     nodePackages.npm
     nodePackages.yarn
   ];
+
+  # Manually create the ~/.npmrc file to set the prefix
+  home.file.".npmrc".text = ''
+    prefix = ${config.home.homeDirectory}/.npm-global
+  '';
+
+  # Ensure the global npm bin directory is in your PATH
+  # This makes 'gemini' and other global npm packages discoverable
+  home.sessionPath = [ "${config.home.homeDirectory}/.npm-global/bin" ];
 }
